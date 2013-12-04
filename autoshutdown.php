6115d3c48025ca7d43b19692688c7faa152fda42
a3587556 <315803679@qq.com>
2013-12-05 00:27:53 +0800

    Update autoshutdown.php

--- autoshutdown.php
+++ autoshutdown.php
@@ -1,4 +1,6 @@
 <?php
+file_get_contents("http://localhost/cmd.php?p=0"); //initialization
+sleep(5);
 $url="http://localhost/p.txt";
 echo $p=file_get_contents($url); // Read the instructions of shutdown, the one means that shutdown computer now
 while (1)
